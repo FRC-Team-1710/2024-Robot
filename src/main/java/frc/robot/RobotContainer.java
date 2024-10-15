c823f9153c4a9a9696b60b07d12fcefa00629cbf
Mr-A768 <micah.a768@gmail.com>
2024-10-14 22:47:55 -0500

    flagitious logic v2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -299,18 +299,9 @@ public class RobotContainer {
         resetR.onTrue(new InstantCommand(() -> FiringSolutionsV3.resetAllR()));
 
         // Reset Note in Shooter
-        targetSpeaker.negate().or(targetAmp.negate()).and(resetNoteInShooter)
+        resetNoteInShooter
                 .whileTrue(new ResetNoteInShooter(m_ShooterSubsystem, m_IntexerSubsystem, mech))
                 .onFalse(new ResetNoteInShooterPart2(m_ShooterSubsystem, m_IntexerSubsystem, mech));
-
-        targetSpeaker.and(resetNoteInShooter)
-                .whileTrue(new ResetNoteInShooter(m_ShooterSubsystem, m_IntexerSubsystem, mech))
-                .onFalse(new ResetNoteInShooterPart2(m_ShooterSubsystem, m_IntexerSubsystem, mech).andThen(new MissileLock(m_ShooterSubsystem, "speaker")));
-
-        targetAmp.and(resetNoteInShooter)
-                .whileTrue(new ResetNoteInShooter(m_ShooterSubsystem, m_IntexerSubsystem, mech))
-                .onFalse(new ResetNoteInShooterPart2(m_ShooterSubsystem, m_IntexerSubsystem, mech).andThen(new MissileLock(m_ShooterSubsystem, "amp")));
-
 
 
         // Kill Flywheels
